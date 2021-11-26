5f412f5255101b9062e3de93b7278e902b4cf6c5
Marek Kuczynski <marek.kuczynski@stedi.com>
2021-11-26 19:55:40 +0100

    Fixed IAM permissions, various fixes

--- lib/cdk-s3-object-lambda-stack.ts
+++ lib/cdk-s3-object-lambda-stack.ts
@@ -6,37 +6,36 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { CfnAccessPoint as s3ObjectAccessPoint } from 'aws-cdk-lib/aws-s3objectlambda';
 import { CfnAccessPoint as s3AccessPoint } from 'aws-cdk-lib/aws-s3';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Tracing } from 'aws-cdk-lib/aws-lambda';
-import { writeFileSync } from 'fs';
+import { Architecture, Tracing } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkS3ObjectLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Create S3 bucket
     const bucket = new Bucket(this, "S3Bucket", {
       encryption: BucketEncryption.S3_MANAGED,
       autoDeleteObjects: true,
       removalPolicy: RemovalPolicy.DESTROY
     });
 
+    // Create S3 convertor Lambda
     const convertLambda = new NodejsFunction(this, "S3ObjectLambda", {
       entry: "./src/lambda/index.ts",
       handler: "handler",
       memorySize: 256,
       retryAttempts: 0,
       timeout: Duration.seconds(5),
-      tracing: Tracing.ACTIVE
+      tracing: Tracing.ACTIVE,
+      architecture: Architecture.ARM_64
     });
 
-    convertLambda.addToRolePolicy(new PolicyStatement({
-      actions: [ "s3-object-lambda:WriteGetObjectResponse" ],
-      resources: [bucket.bucketArn]
-    }));
-    
+    // Create S3 object access point
     const s3AP = new s3AccessPoint(this, "AccessPoint", {
       bucket: bucket.bucketName
     });
 
+    // Create S3 object Lambda access point 
     const objectAP = new s3ObjectAccessPoint(this, 'MyCfnAccessPoint', {
       objectLambdaConfiguration: {
         supportingAccessPoint: s3AP.attrArn,   
@@ -52,7 +51,14 @@ export class CdkS3ObjectLambdaStack extends Stack {
         }]
       }
     });
-    
-    new CfnOutput(this, "AccessPointArn", { value: objectAP.attrArn ?? 'error' });
+
+    // Allow Lambda to write S3 response
+    convertLambda.addToRolePolicy(new PolicyStatement({
+      actions: ["s3-object-lambda:WriteGetObjectResponse"],
+      resources: ["arn:aws:s3-object-lambda:" + this.region + ":" + this.account + ":accesspoint/*"]
+    }));
+
+    // Return S3 Access Point ARN
+    new CfnOutput(this, "AccessPointArn", { value: s3AP.attrArn ?? 'error' });
   }
 }
--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,31 +1,34 @@
 import { S3 } from "aws-sdk";
 import axios from "axios";
 
+// Setup S3 client
 const s3client = new S3({region: process.env.AWS_REGION});
 
 exports.handler = async function (event: any, context: any) {
     
+    // Print input event
     console.log(event);
 
+    // Retrieve values from input event
     const inputS3Url = event.getObjectContext?.inputS3Url;
     const requestRoute = event.getObjectContext?.outputRoute;
     const outputToken = event.getObjectContext?.outputToken;
     
+    // Retrieve signed S3 url from input event
     const object = await axios({
         method: "get",
         url: inputS3Url
     });
 
+    // Convert to uppercase, return file to caller
     await s3client.writeGetObjectResponse({
-        Body: JSON.stringify(object),
+        Body: object.data.toUpperCase(),
         RequestRoute: requestRoute,
         RequestToken: outputToken,
       }).promise();
 
-    console.log(object);
-
+    // Return success
     return { 
         status_code: 200 
     };
-
 };
